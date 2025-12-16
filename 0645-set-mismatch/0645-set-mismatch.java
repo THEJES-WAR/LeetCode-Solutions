c72f10c81c90c98c8efd063da5ee37d9c7a25200
THEJ-D-WAR <vgthejeswar@gmail.com>
2025-12-16 21:33:00 +0530

    Time: 25 ms (6.79%), Space: 48.1 MB (11.32%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] ans = new int[2];
+        Arrays.sort(nums);
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                ans[0] = nums[i];
+            }
+            else set.add(nums[i]);
+        }
+        for(int i=1;i<=nums.length;i++){
+            if(!set.contains(i)){
+                ans[1] = i;
+            }
+        }
+        return ans;
+    }
+}
